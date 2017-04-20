902a0fe68272dc7488410ad6ca942008c6c5205f
gaofeifan <gaofeifan_java@163.com>
2017-04-20 09:37:19 +0800

    搭建框架

---
+++ src/main/java/com/pj/controller/BaseController.java
@@ -0,0 +1,40 @@
+package com.pj.controller;
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.pj.utils.enums.ExceptionEnum;
+/**
+ * 
+ */
+public class BaseController{
+	protected Logger logger = LoggerFactory.getLogger(BaseController.class); 
+	
+	public Map<String,Object> success(Object data){
+		Map<String,Object> datas=new HashMap<String, Object>();
+		datas.put("status",true);
+		datas.put("msg","操作成功");
+		datas.put("code","200");
+		datas.put("data",data);
+		return datas;
+	}
+	
+	public Map<String,Object> error(ExceptionEnum exceptionEnum){
+		Map<String,Object> data=new HashMap<String, Object>();
+		data.put("status",false);
+		data.put("msg",exceptionEnum.getMsg());
+		data.put("code",exceptionEnum.getCode());
+		return data;
+	}
+	
+	public Map<String,Object> error(Object data){
+		Map<String,Object> datas=new HashMap<String, Object>();
+		datas.put("status",false);
+		datas.put("msg",data);
+		datas.put("code","400");
+		datas.put("data","操作失败:"+data);
+		return datas;
+	}
+	
+}
---
+++ src/main/java/com/pj/utils/enums/ExceptionEnum.java
@@ -0,0 +1,31 @@
+package com.pj.utils.enums;
+
+/**
+ *	@author		GFF
+ *	@date		2017年4月13日下午5:31:29
+ *	@version	1.0.0
+ *	@parameter	
+ *  @since		1.8
+ */
+public enum ExceptionEnum {
+
+	HANDLE_EXCEPTION("400" , "处理异常");
+	
+	private String code;
+	private String msg;
+	
+	
+	private ExceptionEnum(String code, String msg) {
+		this.code = code;
+		this.msg = msg;
+	}
+	public String getCode() {
+		return code;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	
+	
+	
+}
