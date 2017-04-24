2fe519163ea515a539c98226a848cebd2b4e486f
gaofeifan <gaofeifan_java@163.com>
2017-04-24 15:31:58 +0800

    修改商品查询

--- src/main/java/com/pj/goods/pojo/ShopGoods.java
+++ src/main/java/com/pj/goods/pojo/ShopGoods.java
@@ -8,175 +8,178 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
-import com.pj.config.page.Page;
-
 import io.swagger.annotations.ApiModelProperty;
 
-@Table(name="shop_goods")
-public class ShopGoods extends Page implements Serializable {
-    @Id
-    @ApiModelProperty(value = "id", required = false)
+@Table(name = "shop_goods")
+public class ShopGoods implements Serializable {
+	@Id
+	@ApiModelProperty(value = "id", required = false)
 	private Integer id;
 
-    /**
-     * 商品名称
-     */
-    @Column
-    @ApiModelProperty(value = "商品名称", required = false)
-    private String goodsName;
+	/**
+	 * 商品名称
+	 */
+	@Column
+	@ApiModelProperty(value = "商品名称", required = false)
+	private String goodsName;
 
-    /**
-     * 商品图片展示路劲
-     */
-    @Column
-    @ApiModelProperty(value = "商品图片展示路劲", required = false	)
-    private String goodsImageUrl;
+	/**
+	 * 商品图片展示路劲
+	 */
+	@Column
+	@ApiModelProperty(value = "商品图片展示路劲", required = false)
+	private String goodsImageUrl;
 
-    /**
-     * 商品数量
-     */
-    @Column
-    @ApiModelProperty(value = "商品数量", required = false)
-    private Integer goodsTotalNum;
+	/**
+	 * 商品数量
+	 */
+	@Column
+	@ApiModelProperty(value = "商品数量", required = false)
+	private Integer goodsTotalNum;
 
-    /**
-     * 商品类型
-     */
-    @Column
-    @ApiModelProperty(value = "商品类型", required = false)
-    private Integer goodsType;
+	/**
+	 * 商品类型
+	 */
+	@Column
+	@ApiModelProperty(value = "商品类型", required = false)
+	private Integer goodsType;
 
-    /**
-     * 商品条形码
-     */
-    @Column
-    @ApiModelProperty(value = "商品条形码", required = false)
-    private String goodsBarcode;
+	/**
+	 * 商品条形码
+	 */
+	@Column
+	@ApiModelProperty(value = "商品条形码", required = false)
+	private String goodsBarcode;
 
-    /**
-     * 商品原价
-     */
-    @Column
-    @ApiModelProperty(value = "商品原价", required = false)
-    private Double goodsOriginalCost;
+	/**
+	 * 商品原价
+	 */
+	@Column
+	@ApiModelProperty(value = "商品原价", required = false)
+	private Double goodsOriginalCost;
 
-    /**
-     * 商品现价
-     */
-    @Column
-    @ApiModelProperty(value = "商品现价", required = false)
-    private Double goodsCurrentPrice;
+	/**
+	 * 商品现价
+	 */
+	@Column
+	@ApiModelProperty(value = "商品现价", required = false)
+	private Double goodsCurrentPrice;
 
-    /**
-     * 商品详情
-     */
-    @Column
-    @ApiModelProperty(value = "商品详情", required = false)
-    private String goodsDetails;
+	/**
+	 * 商品详情
+	 */
+	@Column
+	@ApiModelProperty(value = "商品详情", required = false)
+	private String goodsDetails;
 
-    /**
-     * 商品创建时间
-     */
-    @Column
-    @ApiModelProperty(value = "商品创建时间", required = false)
-    private Date createTime;
+	/**
+	 * 商品创建时间
+	 */
+	@Column
+	@ApiModelProperty(value = "商品创建时间", required = false)
+	private Date createTime;
 
-    /**
-     * 商品修改时间
-     */
-    @Column
-    @ApiModelProperty(value = "商品修改时间", required = false)
-    private Date modifyTime;
+	/**
+	 * 商品修改时间
+	 */
+	@Column
+	@ApiModelProperty(value = "商品修改时间", required = false)
+	private Date modifyTime;
 
-    /**
-     * 	最低价(条件查询)
-     */
-    @Transient
-    @ApiModelProperty(value = "最低价", required = false)
-    private Integer priceMin;
-    
-    /**
-     * 	最高价(条件查询)
-     */
-    @Transient
-    @ApiModelProperty(value = "最高价", required = false)
-    private Integer priceMax;
-    
-    private static final long serialVersionUID = 1L;
+	/**
+	 * 最低价(条件查询)
+	 */
+	@Transient
+	@ApiModelProperty(value = "最低价", required = false)
+	private Integer priceMin;
 
-    public Integer getId() {
-        return id;
-    }
+	/**
+	 * 最高价(条件查询)
+	 */
+	@Transient
+	@ApiModelProperty(value = "最高价", required = false)
+	private Integer priceMax;
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	private Integer pageNo = 1;
 
-    public String getGoodsName() {
-        return goodsName;
-    }
+	private Integer startRow;
 
-    public void setGoodsName(String goodsName) {
-        this.goodsName = goodsName;
-    }
+	private Integer pageSize = 10;
+	private static final long serialVersionUID = 1L;
 
-    public String getGoodsImageUrl() {
-        return goodsImageUrl;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setGoodsImageUrl(String goodsImageUrl) {
-        this.goodsImageUrl = goodsImageUrl;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public Integer getGoodsTotalNum() {
-        return goodsTotalNum;
-    }
+	public String getGoodsName() {
+		return goodsName;
+	}
 
-    public void setGoodsTotalNum(Integer goodsTotalNum) {
-        this.goodsTotalNum = goodsTotalNum;
-    }
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
 
-    public Integer getGoodsType() {
-        return goodsType;
-    }
+	public String getGoodsImageUrl() {
+		return goodsImageUrl;
+	}
 
-    public void setGoodsType(Integer goodsType) {
-        this.goodsType = goodsType;
-    }
+	public void setGoodsImageUrl(String goodsImageUrl) {
+		this.goodsImageUrl = goodsImageUrl;
+	}
 
-    public String getGoodsBarcode() {
-        return goodsBarcode;
-    }
+	public Integer getGoodsTotalNum() {
+		return goodsTotalNum;
+	}
 
-    public void setGoodsBarcode(String goodsBarcode) {
-        this.goodsBarcode = goodsBarcode;
-    }
+	public void setGoodsTotalNum(Integer goodsTotalNum) {
+		this.goodsTotalNum = goodsTotalNum;
+	}
 
-    public Double getGoodsOriginalCost() {
-        return goodsOriginalCost;
-    }
+	public Integer getGoodsType() {
+		return goodsType;
+	}
 
-    public void setGoodsOriginalCost(Double goodsOriginalCost) {
-        this.goodsOriginalCost = goodsOriginalCost;
-    }
+	public void setGoodsType(Integer goodsType) {
+		this.goodsType = goodsType;
+	}
 
-    public Double getGoodsCurrentPrice() {
-        return goodsCurrentPrice;
-    }
+	public String getGoodsBarcode() {
+		return goodsBarcode;
+	}
 
-    public void setGoodsCurrentPrice(Double goodsCurrentPrice) {
-        this.goodsCurrentPrice = goodsCurrentPrice;
-    }
+	public void setGoodsBarcode(String goodsBarcode) {
+		this.goodsBarcode = goodsBarcode;
+	}
 
-    public String getGoodsDetails() {
-        return goodsDetails;
-    }
+	public Double getGoodsOriginalCost() {
+		return goodsOriginalCost;
+	}
 
-    public void setGoodsDetails(String goodsDetails) {
-        this.goodsDetails = goodsDetails;
-    }
+	public void setGoodsOriginalCost(Double goodsOriginalCost) {
+		this.goodsOriginalCost = goodsOriginalCost;
+	}
 
-    public Date getCreateTime() {
+	public Double getGoodsCurrentPrice() {
+		return goodsCurrentPrice;
+	}
+
+	public void setGoodsCurrentPrice(Double goodsCurrentPrice) {
+		this.goodsCurrentPrice = goodsCurrentPrice;
+	}
+
+	public String getGoodsDetails() {
+		return goodsDetails;
+	}
+
+	public void setGoodsDetails(String goodsDetails) {
+		this.goodsDetails = goodsDetails;
+	}
+
+	public Date getCreateTime() {
 		return createTime;
 	}
 
@@ -206,6 +209,36 @@ public class ShopGoods extends Page implements Serializable {
 
 	public void setPriceMax(Integer priceMax) {
 		this.priceMax = priceMax;
+	}
+
+	public void setPageNo(Integer pageNo) {
+		this.pageNo = pageNo;
+		if (pageNo != null) {
+			this.startRow = (pageNo - 1) * this.pageSize;
+		}
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+		if (pageSize != null) {
+			this.startRow = (pageNo - 1) * this.pageSize;
+		}
+	}
+
+	public Integer getStartRow() {
+		return startRow;
+	}
+
+	public void setStartRow(Integer startRow) {
+		this.startRow = startRow;
+	}
+
+	public Integer getPageNo() {
+		return pageNo;
+	}
+
+	public Integer getPageSize() {
+		return pageSize;
 	}
 
 	@Override
